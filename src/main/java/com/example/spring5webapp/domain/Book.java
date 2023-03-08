3f17009c630233dd287d76f8ee42d115cf385f20
Sebastian Suciu <sebastian.suciu@garmin.com>
2023-03-08 16:10:45 +0200

    Lesson 15 - Equality in Hibernate. Added overriedes, generated by IntelliJ, for the equals() and toString() methods.

--- src/main/java/com/example/spring5webapp/domain/Book.java
+++ src/main/java/com/example/spring5webapp/domain/Book.java
@@ -57,4 +57,29 @@ public class Book {
     public Long getId() {
         return id;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
