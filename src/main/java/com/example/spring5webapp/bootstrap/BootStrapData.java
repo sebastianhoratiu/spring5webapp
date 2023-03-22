406002b6ac553c8c6efe545adc206fbe748321fd
Sebastian Suciu <sebastian.suciu@garmin.com>
2023-03-22 11:35:53 +0200

    Assignment 2 - Add publisher entity.

--- src/main/java/com/example/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/com/example/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.example.spring5webapp.bootstrap;
 
 import com.example.spring5webapp.domain.Author;
 import com.example.spring5webapp.domain.Book;
+import com.example.spring5webapp.domain.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,18 @@ public class BootStrapData implements CommandLineRunner {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
 
-    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started in bootstrap");
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
@@ -37,7 +44,15 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(noEJB);
         authorRepository.save(rod);
 
-        System.out.println("Started in bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher publisher = new Publisher("Publisher1", "Street 1", "City 1", "State 1", "50771");
+        publisherRepository.save(publisher);
+
+        Publisher publisher2 = new Publisher("Publisher2", "Street 2", "City 2", "State 2", "50772");
+        publisherRepository.save(publisher2);
+
+        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Publishers: " + publisherRepository.findAll().toString());
     }
 }
